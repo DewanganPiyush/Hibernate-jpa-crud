1bd12c662a9fb7457fc56492c95e1e1bac7a5527
DewanganPiyush <165644916+DewanganPiyush@users.noreply.github.com>
2024-09-18 01:45:10 +0530

    Add files via upload

---
+++ 03-Querying object with JPA/src/main/java/com/learning/cruddemo/CruddemoApplication.java
@@ -0,0 +1,137 @@
+package com.learning.cruddemo;
+
+import com.learning.cruddemo.Entity.Student;
+import com.learning.cruddemo.Entity.dao.StudentDao;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDao studentDao) {
+        return runner -> {
+            //createMultipleStudent(studentDao);
+
+            //readStudent(studentDao);
+
+            //queryForStudents(studentDao);
+
+            //queryForStudentsByLastName(studentDao);
+
+            updateStudent(studentDao);
+        };
+    }
+
+    private void updateStudent(StudentDao studentDao) {
+
+        //retrive student based on the id: primary key
+        int studentId = 1;
+        System.out.println("Getting student with id " + studentId);
+        Student myStudent = studentDao.findById(studentId);
+
+        //change first name to "scooby"
+        System.out.println("Updating student...");
+        myStudent.setFirstName("Scooby");
+
+        //update the student
+        studentDao.update(myStudent);
+
+
+        //display the updated student
+        System.out.println("Updated student" + myStudent);
+    }
+
+    private void queryForStudentsByLastName(StudentDao studentDao) {
+        //create a list of students
+        List<Student> theStudents = studentDao.findByLastName("stokes");
+
+        //display list of students
+        for (Student tempstudent : theStudents) {
+            System.out.println(tempstudent);
+        }
+    }
+
+
+    private void queryForStudents(StudentDao studentDao) {
+        //get a list of students
+        List<Student> theStudents = studentDao.findAll();
+
+        //display a list of students
+        for(Student tempStudent : theStudents) {
+            System.out.println(tempStudent);
+        }
+    }
+
+    private void readStudent(StudentDao studentDao) {
+
+        //create student object
+        System.out.println("creating new student object");
+        Student tempStudent = new Student("donald","duck","duckdonald@gmail.com");
+
+
+        //save the object
+        System.out.println("saving the student...");
+        studentDao.save(tempStudent);
+
+        //display id of the saved student
+        int theId = tempStudent.getId();
+        System.out.println("saved student generated id: " + theId);
+
+        //retrive based on the student id: primary key
+        System.out.println("retrieving student with id: " + theId);
+        Student myStudent = studentDao.findById(theId);
+
+
+        //display student
+        System.out.println("Found the student: " + myStudent);
+    }
+
+    public void createMultipleStudent(StudentDao studentDao) {
+        //create the multiple students
+        System.out.println("creating multiple student object");
+        Student tempStudent1 = new Student("loki","feruksen","loki333@gmail.com");
+        Student tempStudent2 = new Student("ben","stokes","ben67@gmail.com");
+        Student tempStudent3 = new Student("lasi","linga","linga@gmail.com");
+
+        //save the students objects
+        System.out.println("saving the student...");
+        studentDao.save(tempStudent1);
+        studentDao.save(tempStudent2);
+        studentDao.save(tempStudent3);
+
+        //display id of the saved student
+        System.out.println("saved student generated id: " + tempStudent1.getId());
+        System.out.println("saved student generated id: " + tempStudent2.getId());
+        System.out.println("saved student generated id: " + tempStudent3.getId());
+    }
+
+
+
+    public void createStudent(StudentDao studentDao) {
+
+        //create the student object
+        System.out.println("creating new student object");
+        Student tempStudent = new Student("paul","walker","paul123@gmail.com");
+
+        //save the students objects
+        System.out.println("saving the student...");
+        studentDao.save(tempStudent);
+
+        //display id of the saved student
+        System.out.println("saved student generated id: " + tempStudent.getId());
+
+
+
+
+    }
+
+}
---
+++ 03-Querying object with JPA/src/main/java/com/learning/cruddemo/Entity/dao/StudentDAOImpl.java
@@ -0,0 +1,73 @@
+package com.learning.cruddemo.Entity.dao;
+
+import com.learning.cruddemo.Entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDao{
+
+    //define field for entity manager
+    private EntityManager entityManager;
+
+    //inject entity manager using constructor injection
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+
+        //create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("select s from Student s", Student.class);
+
+        //return query result
+
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+
+        //create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName= :theData", Student.class);
+
+        //set query parameters
+        theQuery.setParameter("theData", theLastName);
+
+        //return query results
+        return theQuery.getResultList();
+
+
+    }
+
+    @Override
+    @Transactional //we are updating the database
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+
+
+
+    }
+}
